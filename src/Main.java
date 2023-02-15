888e186d6f034d87f25e80becd3650f2a222f2af
blake <hill2b7@mail.uc.edu>
2023-02-15 12:43:28 -0500

    Lab 06 task 3

--- src/Main.java
+++ src/Main.java
@@ -17,19 +17,19 @@ public class Main
         System.out.println("Enter the length of the rectangle: "); //output to ask the user for their input
         if (in.hasNextDouble()) //if the user's input is a Double then it will execute the code block below
         {
-            length = in.nextDouble();
-            System.out.println("Enter the width of the rectangle: ");
-            if (in.hasNextDouble())
+            length = in.nextDouble(); //sets length equal to the input received
+            System.out.println("Enter the width of the rectangle: "); //output that prompts the user for the width
+            if (in.hasNextDouble()) // verification that input entered is a double
             {
                 width = in.nextDouble(); //sets the double input received to width
                 area = length * width; //formula for area
                 perimeter = length * 2 + width * 2; //formula for perimeter
-                diagonal = Math.sqrt((length * length)+(width*width));
+                diagonal = Math.sqrt((length * length)+(width*width)); //formula for diagonal
                 System.out.println("The area of the rectangle is " + area); //output that tells the user what the area is
                 System.out.println("The perimeter of the rectangle is " + perimeter); //output that tells the user what the perimeter is
-                System.out.println("The diagonal length is " + diagonal);
+                System.out.println("The diagonal length is " + diagonal); //output that tells the user what the diagonal of the rectangle is
             }
-            else
+            else //if the input of the width is incorrect the code block below will run
             {
                 trash = in.nextLine();//sets trash variable to the string input that was received
                 System.out.println("You entered an incorrect value" + trash);// output that tells the user the input was bad
