9466baa9f407940237f787210e06f3153fca91fb
blake <hill2b7@mail.uc.edu>
2023-02-15 12:36:47 -0500

    Lab 06 task 3

--- src/Main.java
+++ src/Main.java
@@ -5,38 +5,40 @@ public class Main
     public static void main(String[] args)
     {
 
-        double length = 0;
-        double width = 0;
-        double area = 0;
-        double perimeter = 0;
-        String trash = "";
+        double length = 0; //variable declaration
+        double width = 0; // variable declaration
+        double area = 0;//variable declaration
+        double perimeter = 0;//variable declaration
+        double diagonal = 0; //variable declaration
+        String trash = "";//String declaration
 
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); //import of Java Scanner
 
-        System.out.println("Enter the length of the rectangle: ");
-        if (in.hasNextDouble())
+        System.out.println("Enter the length of the rectangle: "); //output to ask the user for their input
+        if (in.hasNextDouble()) //if the user's input is a Double then it will execute the code block below
         {
             length = in.nextDouble();
             System.out.println("Enter the width of the rectangle: ");
             if (in.hasNextDouble())
             {
-                width = in.nextDouble();
-                area = length * width;
-                perimeter = length * 2 + width * 2;
-                System.out.println("The area of the rectangle is " + area);
-                System.out.println("The perimeter of the rectangle is " + perimeter);
-
+                width = in.nextDouble(); //sets the double input received to width
+                area = length * width; //formula for area
+                perimeter = length * 2 + width * 2; //formula for perimeter
+                diagonal = Math.sqrt((length * length)+(width*width));
+                System.out.println("The area of the rectangle is " + area); //output that tells the user what the area is
+                System.out.println("The perimeter of the rectangle is " + perimeter); //output that tells the user what the perimeter is
+                System.out.println("The diagonal length is " + diagonal);
             }
             else
             {
-                trash = in.nextLine();
-                System.out.println("You entered an incorrect value" + trash);
+                trash = in.nextLine();//sets trash variable to the string input that was received
+                System.out.println("You entered an incorrect value" + trash);// output that tells the user the input was bad
             }
         }
-        else
+        else // if the input of the length is incorrect, the code block below will run
         {
-            trash = in.nextLine();
-            System.out.println("You entered an incorrect value " + trash);
+            trash = in.nextLine(); //sets trash variable to the string input that was received
+            System.out.println("You entered an incorrect value " + trash); // output that tells the user the input was bad
         }
     }
 }
